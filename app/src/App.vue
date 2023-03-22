791d825abde20d82841e36def2206e0c688773e1
Damien D'ARRAS <damien.d-arras@ensg.eu>
2023-03-22 12:14:40 +0100

    dockerfile

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Welcome to Your Vue.js App 00000000000000000000000" />
   <div id="map" class="map"></div>
 </template>
 
@@ -34,3 +34,6 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+12346987
