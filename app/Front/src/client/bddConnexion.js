907fc3fe0720ac75729100e741d0538f2e91412c
Damien D'ARRAS <damien.d-arras@ensg.eu>
2023-03-24 12:21:08 +0100

    vue-cli is a lot more enjoyable to work with

--- app/Front/src/client/bddConnexion.js
+++ app/Front/src/client/bddConnexion.js
@@ -1,5 +1,5 @@
 export const getAllLiveData = async function getAllLiveData() {
-    return fetch("http://localhost:3000", {
+    return fetch("http://localhost:5500", {
         method: 'GET'
     })
         .then(response => response.json())
