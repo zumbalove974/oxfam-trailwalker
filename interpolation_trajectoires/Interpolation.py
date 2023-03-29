99585576bea300a49467d3b68956546508921278
Damien D'ARRAS <damien.d-arras@ensg.eu>
2023-03-29 17:32:43 +0200

    interpolation functions done

---
+++ interpolation_trajectoires/Interpolation.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import math
+
+def moyennageDF(df_origin):
+    
+    df_rolled = df_origin.copy()
+    df_result = df_rolled.rolling('3min', on="timestamp").mean()
+    
+    df_result['speed', 'device', 'alt'] = df_origin['speed', 'device', 'alt']
+    df_result.drop_duplicates(subset = "timestamp", inplace=True, keep='last')
+    
+    return df_result
+
+
+def interpolationCoord(x, y, df):
+    
+    df_copy = df.copy()
+    df_copy.insert(0, 'dist', (df['x']-x)**2 + (df['y']-y)**2)
+    
+    idxmin = df_copy['dist'].idxmin()
+    return idxmin, math.sqrt(df_copy['dist'][idxmin])
+
+
+def interpolationDF(df_traj, df_ref):
+    
+    df_result = df_traj.copy()
+    
+    average_dist = 0
+    
+    for i in range(len(df_result)):
+        idxmin, mini = interpolationCoord(df_traj['x'][i], df_traj['y'][i], df_ref)
+        df_result['x'][i] = df_ref['x'][idxmin]
+        df_result['y'][i] = df_ref['y'][idxmin]
+        average_dist += mini
+    
+    return df_result, average_dist/len(df_result)
+
+        
