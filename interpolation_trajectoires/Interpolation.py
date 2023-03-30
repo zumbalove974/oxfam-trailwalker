48970d249ab22818643437378cdfa4afa9356b9e
Damien D'ARRAS <damien.d-arras@ensg.eu>
2023-03-30 12:07:57 +0200

    moyennes speeds

--- interpolation_trajectoires/Interpolation.py
+++ interpolation_trajectoires/Interpolation.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import math
 
+def dist(x1, y1, x2, y2):
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
 def moyennageDF(df_origin):
     
     df_rolled = df_origin.copy()
-    df_result = df_rolled.rolling('3min', on="timestamp").mean()
+    df_result = df_rolled.rolling('4min', on="timestamp").mean()
     
-    df_result['speed'] = df_origin['speed']
     df_result['device'] = df_origin['device']
     df_result['alt'] = df_origin['alt']
     df_result.drop_duplicates(
@@ -41,4 +43,24 @@ def interpolationDF(df_traj, df_ref):
     
     return df_result, average_dist/len(df_result)
 
-        
+def calculateSpeeds(df_ip):
+    
+    df_result = df_ip.copy()
+    index_list = list(df_result.index.values)
+    
+    df_result['speed'][index_list[0]] = 0
+    df_result['speed'][index_list[len(index_list)-1]] = 0
+    
+    for i in range(1, len(index_list)-1):
+        total_dist = dist(df_ip['x'][index_list[i]], 
+                          df_ip['y'][index_list[i]],
+                          df_ip['x'][index_list[i+1]],
+                          df_ip['y'][index_list[i+1]]) + \
+                          dist(df_ip['x'][index_list[i]], 
+                          df_ip['y'][index_list[i]],
+                          df_ip['x'][index_list[i-1]],
+                          df_ip['y'][index_list[i-1]])
+        time_diff = df_ip['timestamp'][index_list[i+1]] - df_ip['timestamp'][index_list[i-1]]
+        df_result['speed'][index_list[i]] = total_dist / time_diff.total_seconds()
+    
+    return df_result
