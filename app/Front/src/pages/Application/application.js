6a2b29df60dbac5f5198cb9aec9f97e5eca82039
LinaSaba <106802283+LinaSaba@users.noreply.github.com>
2023-04-12 14:07:34 +0200

    Front: mouseover on visu is available

--- app/Front/src/pages/Application/application.js
+++ app/Front/src/pages/Application/application.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './application.vue'
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
+import Tooltip from 'primevue/tooltip';
 
 const app = createApp(App);
 app.use(PrimeVue).use(ToastService).mount('#app');
+app.directive('tooltip', Tooltip);
