47baeab8b1756bbb202892f2817112bfb0ea7690
Damien D'ARRAS <damien.d-arras@ensg.eu>
2023-04-13 17:02:41 +0200

    econnrefused

--- app/Back/unit_test/inter.test.js
+++ app/Back/unit_test/inter.test.js
@@ -1,6 +1,60 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
-});
+const request = require("supertest");
+const app = require("../app");
 
-//Pour pouvoir utiliser pgmock2:
-//Remplacer router.pool par le client de pgmock2, et faire un client.connect()
+//MOCK
+const PgMock2 = require('pgmock2').default;
+const router = require("../routes/inter");
+const truePool = router.pool;
+
+router.pool = new PgMock2();
+
+router.pool.add('SELECT * FROM public."Interpolation_3883" ORDER BY "index"', [], {
+    rowCount: 2,
+    rows: [
+        {
+            "index": "0",
+            "x": 120558.7126364244,
+            "y": 6432977.242621139,
+            "speed": 0,
+            "timestamp":
+                "2021-03-07T06:00:00.000Z",
+            "interpolation_error": 0,
+            "x_GPS": 120558.7126364244,
+            "y_GPS": 6432977.242621139
+        },
+        {
+            "index": "1",
+            "x": 120580.09203932274,
+            "y": 6432977.84723748,
+            "speed": 0,
+            "timestamp": "2021-03-07T06:00:00.000Z",
+            "interpolation_error": 21.38795056122823,
+            "x_GPS": 120558.7126364244,
+            "y_GPS": 6432977.242621139
+        }
+    ]
+})
+
+router.pool.connect()
+
+describe("Test the root path", () => {
+    test("It should response the GET method", async () => {
+        const response = await request(app).get("/inter/3883")
+        expect(response.statusCode).toBe(200);
+        const body = response._body;
+        console.log(body);
+        expect(body).not.toBeNaN();
+        expect(body).toHaveProperty('length', 2)
+        expect(body[0]).toEqual({
+            index: "0",
+            x: 120558.7126364244,
+            y: 6432977.242621139,
+            speed: 0,
+            timestamp: "2021-03-07T06:00:00.000Z",
+            interpolation_error: 0,
+            x_GPS: 120558.7126364244,
+            y_GPS: 6432977.242621139,
+        });
+
+    })
+});
