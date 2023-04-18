1b90825e0d8496590cb1a8fa8a78008dd1ededa9
LinaSaba <106802283+LinaSaba@users.noreply.github.com>
2023-04-18 10:12:45 +0200

    team page is created

---
+++ app/Front/src/pages/Team/team.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app"> </div>
+  <MenuBar pageName='Accueil' pageURL="home"></MenuBar>
+  <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" size="100rem" shape="circle" />
+  <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/asiyajavayant.png" size="xlarge" shape="circle" />
+  <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/onyamalimba.png" size="xlarge" shape="circle" />
+</template>
+
+<script>
+// Primevue css
+import "primevue/resources/themes/bootstrap4-dark-purple/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+import MenuBar from '../../components/MenuBar.vue'
+import Avatar from 'primevue/avatar';
+//import AvatarGroup from 'primevue/avatargroup';   //Optional for grouping
+
+export default {
+  name: 'App',
+  components: {
+    MenuBar,
+    Avatar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+}
+</style>
