94a010ba5ba61d471270eed72456f5052df473b1
AIT OUMAJOUD Hiba <60844517+hibaa-ai@users.noreply.github.com>
2023-04-20 13:24:21 +0000

    commentaires back : tests

--- app/Back/unit_test/cp.test.js
+++ app/Back/unit_test/cp.test.js
@@ -1,14 +1,19 @@
 const request = require("supertest");
 const app = require("../app");
 
+/* Définition des tests pour l'endpoint '/cp'*/
 
 describe("Test the cp path", () => {
     test("It should response the cp object", async () => {
         const response = await request(app).get("/cp")
+        // Vérification du code de réponse HTTP
         expect(response.statusCode).toBe(200);
         const body = response.body;
+        // Vérification que le corps de la réponse n'est pas NaN
         expect(body).not.toBeNaN();
+        // Vérification que l'objet cp contient 10 éléments
         expect(body).toHaveProperty('length', 10)
+        // Vérification de la valeur du deuxième élément de l'objet cp
         expect(body[1]).toEqual([
             133325.23988534685, 6435602.446513227,
             133486.22446421266, 6435282.187671882,
