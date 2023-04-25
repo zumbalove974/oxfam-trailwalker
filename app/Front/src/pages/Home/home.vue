abc47a0d7c104e77893ac55dab6b2ec4296de72b
LinaSaba <106802283+LinaSaba@users.noreply.github.com>
2023-04-25 11:43:34 +0200

    Front: home page carousel

--- app/Front/src/pages/Home/home.vue
+++ app/Front/src/pages/Home/home.vue
@@ -6,6 +6,27 @@
     <!-- Bouton de redirection -->
     <Button>Aller à la Visualisation </Button>
   </a>
+  <div class="card">
+    <Carousel :value="products" :numVisible="3" :numScroll="3">
+      <template #item="slotProps">
+        <div class="border-1 surface-border border-round m-2 text-center py-5 px-3">
+          <div class="mb-3">
+            <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image"
+              :alt="slotProps.data.name" class="w-6 shadow-2" />
+          </div>
+          <div>
+            <h4 class="mb-1">{{ slotProps.data.name }}</h4>
+            <h6 class="mt-0 mb-3">${{ slotProps.data.price }}</h6>
+            <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" />
+            <div class="mt-5">
+              <Button icon="pi pi-search" rounded class="mr-2" />
+              <Button icon="pi pi-star-fill" rounded severity="success" class="mr-2" />
+            </div>
+          </div>
+        </div>
+      </template>
+    </Carousel>
+  </div>
 </template>
 
 <script>
@@ -26,7 +47,30 @@ export default {
   components: {
     MenuBar,
     Button
-  }
+  },
+  data() {
+    return {
+      products: null,
+      responsiveOptions: [
+        {
+          breakpoint: '1199px',
+          numVisible: 3,
+          numScroll: 3
+        },
+        {
+          breakpoint: '991px',
+          numVisible: 2,
+          numScroll: 2
+        },
+        {
+          breakpoint: '767px',
+          numVisible: 1,
+          numScroll: 1
+        }
+      ]
+    };
+  },
+
 }
 </script>
 
